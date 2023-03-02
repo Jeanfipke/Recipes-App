97ac0d2a644325e93abdc6a4a0e9148361142264
Victor Pinheiro Mello <victormello7003@hotmail.com>
2023-03-02 19:28:26 -0300

    [deploy] - Chamada da api e criando Card das receitas

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Card from '../componentes/Card';
+import { drinksApi, mealsApi } from '../services/api';
 
 function Recipes() {
+  const { pathname } = useLocation();
+  const [meals, setMeals] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  const api = async () => {
+    const resultMeals = await mealsApi();
+    setMeals(resultMeals.meals);
+    const resultDrinks = await drinksApi();
+    setDrinks(resultDrinks.drinks);
+  };
+
+  useEffect(() => {
+    api();
+  }, []);
+
+  const stopArray = 12;
+
   return (
-    <div>Recipes</div>
+    <div>
+      {pathname === '/meals' ? (
+        meals.slice(0, stopArray)
+          .map((meal, idx) => <Card key={ meal.idMeal } param={ meal } idx={ idx } />)
+      ) : (
+        drinks.slice(0, stopArray)
+          .map((drink, idx) => <Card key={ drink.idDrink } param={ drink } idx={ idx } />)
+      )}
+    </div>
   );
 }
 
