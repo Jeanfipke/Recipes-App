c023fa2fd81b5055100b5f33f3870dc5f2c299f5
Jean <jeanfipke@gmail.com>
2023-03-02 17:37:57 -0300

    [refactor] rotas feitas, e refatorado Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import DoneRecipes from './pages/DoneRecipes';
+import Profile from './pages/Profile';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Recipes from './pages/Recipes';
+import RecipesDetails from './pages/RecipesDetails';
+import RecipesInProgress from './pages/RecipesInProgress';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route path="/meals/:id-da-receita/in-progress" component={ RecipesInProgress } />
+      <Route path="/drinks/:id-da-receita/in-progress" component={ RecipesInProgress } />
+      <Route path="/meals/:id-da-receita" component={ RecipesDetails } />
+      <Route path="/drinks/:id-da-receita" component={ RecipesDetails } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/drinks" component={ Recipes } />
+      <Route path="/meals" component={ Recipes } />
+      <Route path="/profile" component={ Profile } />
+      <Route exact path="/" component={ Login } />
+      <Route path="/*" component={ NotFound } />
+    </Switch>
   );
 }
 
