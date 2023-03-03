2d47c8fe6d35ab3ab75591810203f972f4a9227b
Jean <jeanfipke@gmail.com>
2023-03-03 14:48:45 -0300

    [refactor] testes adicionados a pagina de perfil

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouter } from './helpers/renderWith';
+
 
 describe('Testa a página de perfil', () => {
   test('Todos os elemento devem estar na página', async () => {
@@ -9,6 +12,31 @@ describe('Testa a página de perfil', () => {
     history.push('/profile');
     expect(history.location.pathname).toBe('/profile');
     const links = await screen.findAllByRole('link');
+    await screen.findByRole('link', { name: /done recipes/i });
+    await screen.findByRole('link', { name: /favorite recipes/i });
+    await screen.findByRole('link', { name: /logout/i });
     expect(links).toHaveLength(3);
+  });
+  test('', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/profile');
+    expect(history.location.pathname).toBe('/profile');
+    const doneRecipesBtn = await screen.findByRole('link', { name: /done recipes/i });
+    const favoriteRecipesBtn = await screen.findByRole('link', { name: /favorite recipes/i });
+    const lougoutBtn = await screen.findByRole('link', { name: /logout/i });
+    act(async () => {
+      userEvent.click(doneRecipesBtn);
+    });
+    expect(history.location.pathname).toBe('/done-recipes');
+    history.push('/profile');
+    act(async () => {
+      userEvent.click(favoriteRecipesBtn);
+    });
+    expect(history.location.pathname).toBe('/favorite-recipes');
+    history.push('/profile');
+    act(async () => {
+      userEvent.click(lougoutBtn);
+    });
+    expect(history.location.pathname).toBe('/');
   });
 });
