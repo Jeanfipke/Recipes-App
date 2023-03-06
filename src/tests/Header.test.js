b00c905a64ac3ff871b97c13fc3055b4023830b3
Jean <jeanfipke@gmail.com>
2023-03-06 19:29:50 -0300

    [WIP] testes Header

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,47 +1,42 @@
-// import React from 'react';
-// import { screen, waitFor } from '@testing-library/react';
-// import { act } from 'react-dom/test-utils';
-// import App from '../App';
-// import { renderWithRouterAndRedux } from './helpers/renderWith';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
 
-// describe('Testa o componente Header', () => {
-//   test('Testa se o header está presente nas páginas necessárias', async () => {
-//     const { history } = renderWithRouterAndRedux(<App />);
+describe('Testa o componente Header', () => {
+  test('Testa se o header está presente nas páginas necessárias', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const altText = 'profile icon';
 
-//     await act(async () => {
-//       await history.push('/meals');
-//     });
-//     const profileIcon = await screen.findByAltText('profile icon');
-//     expect(profileIcon).toBeInTheDocument();
+    await act(async () => {
+      await history.push('/meals');
+    });
+    await screen.findByAltText(altText);
 
-//     await act(async () => {
-//       history.push('/drinks');
-//     });
-//     expect(profileIcon).toBeInTheDocument();
+    await act(async () => {
+      history.push('/drinks');
+    });
+    await screen.findByAltText(altText);
 
-//     await act(async () => {
-//       history.push('/profile');
-//     });
-//     await waitFor(() => expect(profileIcon).toBeInTheDocument());
+    await act(async () => {
+      history.push('/profile');
+    });
+    await screen.findByAltText(altText);
 
-//     await act(async () => {
-//       history.push('/favorite-recipes');
-//     });
-//     expect(profileIcon).toBeInTheDocument();
+    await act(async () => {
+      history.push('/favorite-recipes');
+    });
+    await screen.findByAltText(altText);
 
-//     await act(async () => {
-//       history.push('/done-recipes');
-//     });
-//     expect(profileIcon).toBeInTheDocument();
+    await act(async () => {
+      history.push('/done-recipes');
+    });
+    await screen.findByAltText(altText);
 
-//     await act(async () => {
-//       history.push('/recipes');
-//     });
-//     expect(profileIcon).toBeInTheDocument();
-
-//     await act(async () => {
-//       history.push('/');
-//     });
-//     expect(profileIcon).not.toBeInTheDocument();
-//   });
-// });
+    await act(async () => {
+      history.push('/recipes');
+    });
+    await screen.findByAltText(altText);
+  });
+});
