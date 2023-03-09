72cbc8b62baa2ba66281e587a88c73c8d1b8473a
Marcos <bonaccorsimarcos@gmail.com>
2023-03-09 15:07:19 -0300

    [Fix] Implementação dos elementos da barra de busca

--- src/componentes/SearchBar.jsx
+++ src/componentes/SearchBar.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <div data-testid="search-input">SearchBar</div>
+    <form>
+      <label htmlFor="search">
+        <input id="search" data-testid="search-input" type="text" placeholder="Searc" />
+      </label>
+      <section>
+        <label>
+          Ingredient
+          <input
+            data-testid="ingredient-search-radio"
+            type="radio"
+            value="Ingredient"
+            name="searchFilter"
+          />
+        </label>
+        <label>
+          Name
+          <input
+            data-testid="name-search-radio"
+            type="radio"
+            value="Name"
+            name="searchFilter"
+          />
+        </label>
+        <label>
+          First letter
+          <input
+            data-testid="first-letter-search-radio"
+            type="radio"
+            value="First letter"
+            name="searchFilter"
+          />
+        </label>
+      </section>
+      <button type="submit" data-testid="exec-search-btn">Search</button>
+    </form>
   );
 }
 
